9ccdd266d964a7af21b7b53e04ff2f22ea5add48
Wijk Elias <elias.wijk@edu.ystad.se>
2019-10-23 13:40:38 +0200

    Started implementing map gen, scrap later
    Start working on better map gen next time, current one is very rudimentary and not good enough

--- Assets/Map Generator/LevelGeneration.cs
+++ Assets/Map Generator/LevelGeneration.cs
@@ -4,101 +4,28 @@ using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour
 {
-    public Transform[] startingPositions;
-    public GameObject[] rooms; // 0 -> LR, 1 -> LRB, 2 -> LRT, 3 -> LRTB
-
-    private int direction;
-    public float moveAmount;
-
-    private float timeBtwRoom;
-    public float startTimeBtwRoom = 0.25f;
-
-    public float minX;
-    public float maxX;
-    public float minY;
-    private bool stopGeneration;
-
-    public LayerMask room;
-
-    private void Start()
-    {
-        int randStartingPos = Random.Range(0, startingPositions.Length);
-        transform.position = startingPositions[randStartingPos].position;
-        Instantiate(rooms[0], transform.position, Quaternion.identity);
-
-        direction = Random.Range(1, 6);
+    private int direction; // 1UP 2RIGHT 3DOWN 4LEFT
+    public float btwMove = 0.02f;
+    public float moveAmount = 1f;
+    public GameObject floorTile;
+    private void Start() {
+        direction = Random.Range(1, 5);
+        Instantiate(floorTile, transform.position, Quaternion.identity);
+        Move();
     }
 
-    private void Update()
-    {
-        if(timeBtwRoom <= 0 && stopGeneration == false) {
-            Move();
-            timeBtwRoom = startTimeBtwRoom;
-        } else {
-            timeBtwRoom -= Time.deltaTime;
-        }
+    private void Move() {
+        if (direction == 1) transform.position += Vector3.up * moveAmount;
+        if (direction == 2) transform.position += Vector3.right * moveAmount;
+        if (direction == 3) transform.position += Vector3.down * moveAmount;
+        if (direction == 4) transform.position += Vector3.left * moveAmount;
+        findNewDir(direction);
+        Instantiate(floorTile, transform.position, Quaternion.identity);
+        Invoke("Move", btwMove);
     }
 
-    private void Move()
-    {
-        if (direction == 1 || direction == 2) { // Move RIGHT !
-            if (transform.position.x < maxX) {
-                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
-                transform.position = newPos;
-
-                int rand = Random.Range(0, rooms.Length);
-                Instantiate(rooms[rand], transform.position, Quaternion.identity);
-
-                direction = Random.Range(1, 6);
-                if (direction == 3) {
-                    direction = 2;
-                } else if (direction == 4) {
-                    direction = 5;
-                }
-            }
-            else {
-                direction = 5;
-            }
-
-        } else if(direction == 3 || direction == 4) { // Move LEFT !
-            if (transform.position.x > minX) {
-                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
-                transform.position = newPos;
-
-                int rand = Random.Range(0, rooms.Length);
-                Instantiate(rooms[rand], transform.position, Quaternion.identity);
-
-                direction = Random.Range(3, 6);
-            }
-            else {
-                direction = 5;
-            }
-
-        }  else if (direction == 5) { //Move DOWN !
-            if (transform.position.y > minY) {
-                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
-                if(roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3) {
-                    roomDetection.GetComponent<RoomType>().RoomDestruction();
-
-                    int randBottomRoom = Random.Range(1, 4);
-                    if (randBottomRoom == 2) {
-                        randBottomRoom = 1;
-                    }
-                    Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
-                }
-
-
-                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
-                transform.position = newPos;
-
-                int rand = Random.Range(2, 4);
-                Instantiate(rooms[rand], transform.position, Quaternion.identity);
-
-                direction = Random.Range(1, 6);
-            }
-            else {
-                stopGeneration = true;
-            }
-        }
+    private void findNewDir(int oldDir) {
+        direction = Random.Range(1, 5);
+        if(direction == oldDir) findNewDir(oldDir);
     }
 }
